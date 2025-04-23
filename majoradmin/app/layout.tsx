65c6f5b400056fdc4e136a20210d972cbed0efde
Sanghrakshit12 <gautamsanghrakshit@gmail.com>
2025-04-23 15:22:19 +0530

    admin

--- majoradmin/app/layout.tsx
+++ majoradmin/app/layout.tsx
@@ -1,14 +1,20 @@
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import '@mantine/tiptap/styles.css';
+import Header from './Components/Header/Header';
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
+      <body className="bg-white pt-16">
+        <Header />
+        <MantineProvider defaultColorScheme="light">
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
